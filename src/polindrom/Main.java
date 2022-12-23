ebd97ae52408fc052a86c82e36e5a3fd77d65ef1
AzaMurat <=>
2022-12-23 23:05:13 +0300

    Initial commit

---
+++ src/polindrom/Main.java
@@ -0,0 +1,14 @@
+package polindrom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(122));
+    }
+        public static boolean isPalindrome(int x) {
+            if (x >= 0) {
+                int a = Integer.parseInt(new StringBuilder(String.valueOf(x)).reverse().toString());
+                return a == x;
+            } else
+                return false;
+        }
+}
