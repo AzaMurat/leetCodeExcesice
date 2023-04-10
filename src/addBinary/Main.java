3edfc2db909350659ff095247145d65a42b80b8a
AzaMurat <=>
2023-04-10 23:48:43 +0300

    Add Binary tasks!!!

---
+++ src/addBinary/Main.java
@@ -0,0 +1,38 @@
+package addBinary;
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "11";
+        String b = "1";
+        String sum = addBinary(a, b);
+        System.out.println(sum);
+
+    }
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        if (carry != 0) {
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
+    }
+
+}
